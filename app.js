f7ae7c195790612dd6f72fbaae06623da9c81981
Cameron Lee <camdlee18@gmail.com>
2023-07-15 18:18:51 -0400

    created variables, added event listener for counter change

---
+++ app.js
@@ -0,0 +1,32 @@
+//------------Variables--------------
+let count = 0
+
+
+// -----------Selections------------
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn')
+
+
+// ----------Adding function to each button------------
+btns.forEach((btn)=>{
+    //for each button, we want to a create event to happen when it is clicked
+    btn.addEventListener('click', (event)=>{
+        //just to make sure we see what happens when we click the button
+        //console.log(event.currentTarget);
+        //console.log(event.currentTarget.classList)
+
+        // saving the div class to a variable
+        const styles = event.currentTarget.classList;
+        //if function to assess whether to decrease, reset, or increase the counter
+        if(styles.contains('decrease')){
+            count--
+            value.innerHTML = count
+        } else if (styles.contains('increase')){
+            count++
+            value.innerHTML = count
+        } else {
+            count = 0
+            value.innerHTML = count
+        }
+    })
+})
